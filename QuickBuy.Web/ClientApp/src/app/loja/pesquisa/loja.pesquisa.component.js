9d35ada5e6d177258a45912eb0870babf6b93603
Andre Silva <andrebhdev@gmail.com>
2021-05-04 16:32:42 -0300

    Construindo o template para exibição dos produtos na loja

---
+++ QuickBuy.Web/ClientApp/src/app/loja/pesquisa/loja.pesquisa.component.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LojaPesquisaComponent = void 0;
+var core_1 = require("@angular/core");
+core_1.Component({
+    selector: 'app-loja',
+    templateUrl: './loja.pesquisa.component.html',
+    styleUrls: ['./loja.pesquisa.component.css']
+});
+var LojaPesquisaComponent = /** @class */ (function () {
+    function LojaPesquisaComponent(produtoServico) {
+        this.produtoServico = produtoServico;
+    }
+    LojaPesquisaComponent.prototype.ngOnInit = function () {
+    };
+    return LojaPesquisaComponent;
+}());
+exports.LojaPesquisaComponent = LojaPesquisaComponent;
+//# sourceMappingURL=loja.pesquisa.component.js.map
