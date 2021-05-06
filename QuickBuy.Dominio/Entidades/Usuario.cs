49a725ef8e9582b434fa84c792e4d8ef418ac9cd
Andre Silva <andrebhdev@gmail.com>
2021-05-06 17:35:15 -0300

    Criando o usuario Admin

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -9,6 +9,7 @@ namespace QuickBuy.Dominio.Entidades
         public string Senha { get; set; }
         public string Nome { get; set; }
         public string SobreNome { get; set; }
+        public bool EhAdministrador { get; set; }
 
         /// <summary>
         /// Um Usuario pode ter um ou muitos itens de pedidos
